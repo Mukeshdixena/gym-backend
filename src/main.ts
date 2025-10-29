c4e4c41b84d617c5c4fb6df3693ba574d873931e
mukesh <mukeshdixena123@gmail.com>
2025-10-29 13:49:35 +0530

    add public bound"

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     credentials: true, // optional, if you use cookies/auth
   });
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 
