12dfbb95fe5bdfa9d6835aa3fbe506d4ae211ee6
mukesh <mukeshdixena123@gmail.com>
2025-10-29 13:35:56 +0530

    log port

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,9 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true, // optional, if you use cookies/auth
   });
-
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 
 bootstrap().catch((err) => {
